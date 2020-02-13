f66aa379e06b53389f511261e44b2ec2fd0a6331
ajabar921 <jabarkhugyani@yahoo.com>
2020-02-12 19:43:02 -0600

    first commit

--- GIT1Project/src/Git1Practice.java
+++ GIT1Project/src/Git1Practice.java
@@ -3,7 +3,7 @@ public class Git1Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1st change");
+		System.out.println("1st changed");
 	}
 
 }
